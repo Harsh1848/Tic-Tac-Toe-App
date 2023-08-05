9b5167b5d1fc2445db4807059064525143a3fe0d
Harsh1848 <harsh.hp7999@gmail.com>
2023-08-04 22:02:50 -0400

    created controller for the game

---
+++ Controllers/GameController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tic_Tac_Toe_App.Controllers
+{
+    public class GameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
