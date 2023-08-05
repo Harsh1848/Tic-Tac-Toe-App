a118493dfa46fdc59ac8892652df60d36b22ea8e
Harsh1848 <harsh.hp7999@gmail.com>
2023-08-04 22:21:44 -0400

    created a model for the code

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Tic_Tac_Toe_App.Models;
 
 namespace Tic_Tac_Toe_App.Controllers
 {
     public class GameController : Controller
     {
+        private readonly GameBoard _gameBoard;
+
+        public GameController()
+        {
+            _gameBoard = new GameBoard();
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View("~/Views/Home/Index.cshtml", _gameBoard);
+        }
+
+        [HttpPost]
+        public IActionResult MakeMove(int row, int col)
+        {
+            if (_gameBoard.MakeMove(row, col))
+            {
+                // Implement win/draw check here
+            }
+            return RedirectToAction("Index");
         }
     }
 }
--- Models/GameBoard.cs
+++ Models/GameBoard.cs
@@ -1,19 +1,20 @@
 ﻿namespace Tic_Tac_Toe_App.Models
-{
+{// Models/GameBoard.cs
     public class GameBoard
     {
-        public char[,] Board { get; set; } = new char[3, 3]; // Represents the game board
-        public char CurrentPlayer { get; set; } = 'X'; // 'X' starts the game
+        public char[,] Board { get; set; } = new char[3, 3];
+        public char CurrentPlayer { get; set; } = 'X';
 
         public bool MakeMove(int row, int col)
         {
             if (Board[row, col] == '\0')
             {
                 Board[row, col] = CurrentPlayer;
-                CurrentPlayer = (CurrentPlayer == 'X') ? 'O' : 'X'; // Switch players
+                CurrentPlayer = (CurrentPlayer == 'X') ? 'O' : 'X';
                 return true;
             }
-            return false; // Invalid move
+            return false;
         }
     }
+
 }
