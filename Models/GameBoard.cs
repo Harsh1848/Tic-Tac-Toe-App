02acb5a6ac1c151877fb6fcaba441ee6ee35b715
Harsh1848 <harsh.hp7999@gmail.com>
2023-08-04 22:00:10 -0400

    Add project files.

---
+++ Models/GameBoard.cs
@@ -0,0 +1,19 @@
+﻿namespace Tic_Tac_Toe_App.Models
+{
+    public class GameBoard
+    {
+        public char[,] Board { get; set; } = new char[3, 3]; // Represents the game board
+        public char CurrentPlayer { get; set; } = 'X'; // 'X' starts the game
+
+        public bool MakeMove(int row, int col)
+        {
+            if (Board[row, col] == '\0')
+            {
+                Board[row, col] = CurrentPlayer;
+                CurrentPlayer = (CurrentPlayer == 'X') ? 'O' : 'X'; // Switch players
+                return true;
+            }
+            return false; // Invalid move
+        }
+    }
+}
